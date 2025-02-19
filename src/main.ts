2268ffb936705295cacd0a41c2655d0a2473e547
or-abdillh <hans.abdillh05@gmail.com>
2025-02-19 18:07:55 +0800

    chore: enable CORS with detailed configuration for methods and headers

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        "https://hulu.dtz-internal-only.com",
-        "https://app.hulutarget.com",
-        "http://localhost:8000",
-        "http://localhost:5173"
-      ]
-    }
-  });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "https://hulu.dtz-internal-only.com",
+      "https://app.hulutarget.com",
+      "http://localhost:8000",
+      "http://localhost:5173"
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  })
 
   await app.listen(process.env.PORT ?? 3000);
 }
