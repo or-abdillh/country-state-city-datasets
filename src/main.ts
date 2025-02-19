8aea894a22c910dd9e6fc63b6b66ab4186874675
or-abdillh <hans.abdillh05@gmail.com>
2025-02-19 18:23:51 +0800

    chore: refactor CORS configuration to use a dynamic origin check and update allowed headers

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'https://hulu.dtz-internal-only.com',
+    'https://app.hulutarget.com',
+    'http://localhost:8000',
+    'http://localhost:5173'
+  ];
+
   app.enableCors({
-    origin: [
-      "https://hulu.dtz-internal-only.com",
-      "https://app.hulutarget.com",
-      "http://localhost:8000",
-      "http://localhost:5173"
-    ],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
     credentials: true,
   })
 
