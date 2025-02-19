e8de56d41480a07754aaaf1203ad04bf061b25b2
or-abdillh <hans.abdillh05@gmail.com>
2025-02-19 18:08:01 +0800

    chore: implement CORS middleware in AppModule for all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountryStateCityModule } from './modules/country-state-city/country-state-city.module';
 import { CountryStateCityController } from './controllers/country-state-city/country-state-city.controller';
 import { CountryStateCityService } from './services/country-state-city/country-state-city.service';
+import { CorsMiddleware } from './middlewares/cors.middlware';
 
 @Module({
   imports: [CountryStateCityModule],
   controllers: [AppController, CountryStateCityController],
   providers: [AppService, CountryStateCityService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes("*")
+  }
+}
