0de21c576cff8e559ae3821fdff66a50b1dc394e
or-abdillh <hans.abdillh05@gmail.com>
2025-02-19 17:30:41 +0800

    chore: update CORS configuration to allow specific origins

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    credentials: true,
+    origin: [
+      "https://hulu.dtz-internal-only.com",
+      "https://app.hulutarget.com",
+      "http://localhost:8000",
+      "http://localhost:5173"
+    ]
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
