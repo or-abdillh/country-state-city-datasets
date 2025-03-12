cd63ccbe97b3ea6ba469eea6a5f31ac4142474d6
or-abdillh <hans.abdillh05@gmail.com>
2025-03-12 09:35:15 +0800

    chore: add additional localhost origins to CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
     'https://hulu.dtz-internal-only.com',
     'https://app.hulutarget.com',
     'http://localhost:8000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:5173'
   ];
 
   app.enableCors({
--- src/middlewares/cors.middlware.ts
+++ src/middlewares/cors.middlware.ts
@@ -8,7 +8,9 @@ export class CorsMiddleware implements NestMiddleware {
             'https://hulu.dtz-internal-only.com',
             'https://app.hulutarget.com',
             'http://localhost:8000',
-            'http://localhost:5173'
+            'http://localhost:5173',
+            'http://127.0.0.1:8000',
+            'http://127.0.0.1:5173'
         ];
 
         const origin = req.headers.origin;
