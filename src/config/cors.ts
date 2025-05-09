83187f6d11befa3ad44b950549a3865e0d719c77
or-abdillh <hans.abdillh05@gmail.com>
2025-05-09 09:37:41 +0800

    chore: add CORS configuration for allowed origins

---
+++ src/config/cors.ts
@@ -0,0 +1,12 @@
+export default {
+    origins: [
+        'https://hulu.dtz-internal-only.com',
+        'https://app.hulutarget.com',
+        'https://singularity.corpodeus.com',
+        'https://corpodeus.com',
+        'http://localhost:8000',
+        'http://localhost:5173',
+        'http://127.0.0.1:8000',
+        'http://127.0.0.1:5173'
+    ]
+}
