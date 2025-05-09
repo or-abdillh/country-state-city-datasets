4659c3bf5c0585e73584090bde447f518344eaf4
or-abdillh <hans.abdillh05@gmail.com>
2025-05-09 09:37:24 +0800

    chore: remove unused CountryStateCity module and related components from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CountryStateCityModule } from './modules/country-state-city/country-state-city.module';
-import { CountryStateCityController } from './controllers/country-state-city/country-state-city.controller';
-import { CountryStateCityService } from './services/country-state-city/country-state-city.service';
 import { CorsMiddleware } from './middlewares/cors.middlware';
 
 @Module({
-  imports: [CountryStateCityModule],
-  controllers: [AppController, CountryStateCityController],
-  providers: [AppService, CountryStateCityService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
